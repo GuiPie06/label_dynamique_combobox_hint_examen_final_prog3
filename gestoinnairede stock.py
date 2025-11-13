583c4aa023bc9e74947d35d64b5a101d3f098390
6245102 <6245102@ctsjean.qc.ca>
2025-11-13 14:46:09 -0500

    0

--- gestoinnairede stock.py
+++ gestoinnairede stock.py
@@ -39,26 +39,71 @@ class Gestionnaire_Stock(tk.Tk):
         self.label_titre.grid(row=2, column=0, sticky="w")
         self.label_auteur=(ttk.Label(self.labelFrame_fiche_article, text="Auteur"))
         self.label_auteur.grid(row=3, column=0, sticky="w")
-        self.label_quantite=(ttk.Label(self.labelFrame_fiche_article, text="Quantité à acheter"))
-        self.label_quantite.grid(row=4, column=0, sticky="w")
+
+
+
+
+
+
 
         self.statut_combo = tk.StringVar(value="Achat")
-        self.entry_statut = ttk.Combobox(self.labelFrame_fiche_article, values=("Achat", "Vente"), state="readonly", textvariable=self.statut_combo )
-        self.entry_statut.grid(row=0, column=1, sticky="e")
+        self.combo_statut = ttk.Combobox(self.labelFrame_fiche_article, values=["Achat", "Vente"], state="readonly", textvariable=self.statut_combo )
+        self.combo_statut.grid(row=0, column=1, sticky="e")
+
+
         self.ent_ISBN = ttk.Entry(self.labelFrame_fiche_article, )
         self.ent_ISBN.grid(row=1, column=1, sticky="e")
         self.ent_titre = ttk.Entry(self.labelFrame_fiche_article, )
         self.ent_titre.grid(row=2, column=1, sticky="e")
         self.ent_auteur = ttk.Entry(self.labelFrame_fiche_article, )
         self.ent_auteur.grid(row=3, column=1, sticky="e")
-        self.ent_quantite = ttk.Entry(self.labelFrame_fiche_article, )
-        self.ent_quantite.grid(row=4, column=1, sticky="e")
 
-        if self.entry_statut.get == "Achat":
-            self.label_statut.configure(text="Quantité à acheter")
 
-        if self.entry_statut.get == "Vente":
-            self.label_statut.configure(text="Quantité à vendre")
+
+
+        self.label_quantite_acheter=(ttk.Label(self.labelFrame_fiche_article, text="Quantité à acheter"))
+        self.label_quantite_acheter.grid(row=4, column=0, sticky="w")
+        self.ent_quantite_acheter = ttk.Entry(self.labelFrame_fiche_article, )
+        self.ent_quantite_acheter.grid(row=4, column=1, sticky="e")
+
+        self.label_quantite_vendre=(ttk.Label(self.labelFrame_fiche_article, text="Quantité à vendre"))
+        self.label_quantite_vendre.grid(row=4, column=0, sticky="w")
+        self.ent_quantite_vendre = ttk.Entry(self.labelFrame_fiche_article, )
+        self.ent_quantite_vendre.grid(row=4, column=1, sticky="e")
+
+
+        self.combo_statut.bind("<<ComboboxSelected>>", self.changer_statut)
+
+    def changer_statut(self, event=None):
+        self.update_champs()
+
+    def update_champs(self):
+        if  self.statut_combo.get() == "Achat":
+            self.label_quantite_acheter.grid()
+            self.ent_quantite_acheter.grid()
+            self.label_quantite_vendre.grid_remove()
+            self.ent_quantite_vendre.grid_remove()
+
+        if self.statut_combo.get() == "Vente":
+            self.label_quantite_vendre.grid()
+            self.ent_quantite_vendre.grid()
+            self.label_quantite_acheter.grid_remove()
+            self.ent_quantite_acheter.grid_remove()
+
+        else:
+            return
+
+
+        # if self.entry_statut.get == "Achat":
+        #     self.label_statut.configure(text="Quantité à acheter")
+        #
+        # if self.entry_statut.get == "Vente":
+        #     self.label_statut.configure(text="Quantité à vendre")
+
+
+
+
+
 
         self.labelFrame_outils_gestion = ttk.LabelFrame(self.labelFrame_fiche_article, text="Outils de gestion")
         self.labelFrame_fiche_article.grid(row=5, column=0, columnspan=2, sticky="nswe")
